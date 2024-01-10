61d8d2a711d69a65d3380ddd1d2b596c8aa321b2
Maitrish Mukherjee <maitrish03@gmail.com>
2024-01-10 20:23:47 +0530

    remove single

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Users from "./pages/users/Users";
 import Products from "./pages/products/Products";
 import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
 import Menu from "./components/menu/Menu";
 import Login from "./pages/login/Login";
 import "./styles/global.scss";
@@ -32,7 +31,6 @@ function App() {
             </QueryClientProvider>
           </div>
         </div>
-        <Footer />
       </div>
     );
   };
